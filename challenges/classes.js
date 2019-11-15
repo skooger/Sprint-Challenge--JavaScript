ce2c79b5d1123f41cceb16fa47bd6694335355f6
skooger <adamjskooglambda@gmail.com>
2019-11-15 09:39:52 -0800

    completed the class stretch goal and fixed the reduce function

--- challenges/classes.js
+++ challenges/classes.js
@@ -4,4 +4,31 @@
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
+//Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  
+//Test your work by logging out your volume and surface area.
+class CubeMaker{
+    constructor(length,width,height){
+        this.length = length;
+        this.width = width;
+        this.height = height;
+      }
+}
+
+
+class CuboidMaker extends CubeMaker{
+    constructor(length,width,height){
+        super(length,width,height);
+    }
+    volume(){
+        return this.length * this.width * this.height;
+    }
+    surfaceArea(){
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
+      
+}
+  
+const cuboid = new CuboidMaker(4,5,5);
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -145,8 +145,9 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(zooAnimals.population);
+let populationTotal = 0;
+let initialValue = 0;
+populationTotal = zooAnimals.reduce((accumulator, currentValue) => accumulator + currentValue.population, initialValue)
 console.log(populationTotal);
 
 
